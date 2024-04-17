a10b6887381b6f61adc1e5d5f37dc399f212f021
dima <martin.1.ma@nokia-sbell.com>
2024-04-17 10:09:50 +0000

    Add pylsp for flake8 and pylint

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,29 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                -- formatter options
+                black = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+                flake8 = { enabled = true },
+                -- linter options
+                pylint = { enabled = true, args = { "--max-line-length", "120" } },
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+                -- type checker
+                pylsp_mypy = { enabled = true },
+                -- auto-completion options
+                jedi_completion = { fuzzy = true },
+                -- import sorting
+                pyls_isort = { enabled = true },
+              },
+            },
+          },
+        },
         clangd = {
           cmd = {
             "clangd",

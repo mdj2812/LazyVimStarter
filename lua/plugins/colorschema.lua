a8086c6ceadf514fd066e5e5733866a7c6e02983
dima <martin.1.ma@nokia-sbell.com>
2024-04-28 07:14:58 +0000

    Use cyberdream as colorschema

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,12 +1,25 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        -- Recommended - see "Configuring" below for more config options
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      })
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "cyberdream",
     },
   },
 }

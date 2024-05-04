3104ae9311f1b0083b70fcf315db0219d0e78282
mdj2812 <mdj2812@hotmail.com>
2024-05-05 02:06:48 +0800

    Configure Oil options

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,18 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+    win_options = {
+      wrap = true,
+      list = true,
+    },
+    experimental_watch_for_changes = true,
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }

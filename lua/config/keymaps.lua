58f5c11441d753304ea2dfa2b989e6a34e750347
mdj2812 <mdj2812@hotmail.com>
2024-05-05 01:24:18 +0800

    Add oil.nvim for filesystem editing
    - add keymaps to call Oil

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "-", "<cmd>Oil<cr>", { desc = "Oil (cwd)" })
+map("n", "<C-o>", "<cmd>Oil --float<cr>", { desc = "Oil Floating (cwd)" })

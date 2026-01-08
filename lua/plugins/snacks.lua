79deb4a782777e923687a82472eb1f1bf808057d
mdj2812 <mdj2812@hotmail.com>
2026-01-08 21:20:15 +0800

    Add border to Lazygit

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    styles = {
+      lazygit = {
+        border = true,
+      },
+      float = {
+        border = true,
+      },
+    },
+  },
+}

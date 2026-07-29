b3258f93a2ebd2ec0f918c65c5ab70537105849b
dima <martin.1.ma@nokia.com>
2026-07-29 15:23:07 +0800

    fix theme to catppuccin

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,48 +1,14 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim" },
-
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
     opts = {
       background = {
         light = "latte",
         dark = "mocha",
       },
-      transparent_background = true,
-      float = {
-        transparent = true,
-        solid = false,
-      },
     },
   },
-
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = {
-  --     style = "storm",
-  --     transparent = true,
-  --   },
-  -- },
-
-  -- {
-  --   "scottmckendry/cyberdream.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("cyberdream").setup({
-  --       -- Recommended - see "Configuring" below for more config options
-  --       transparent = true,
-  --       italic_comments = true,
-  --       hide_fillchars = true,
-  --       borderless_telescope = true,
-  --       terminal_colors = true,
-  --     })
-  --   end,
-  -- },
 
   {
     "LazyVim/LazyVim",
